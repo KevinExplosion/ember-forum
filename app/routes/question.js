72864cef2aac1311c1d799cb9a0276de3f09d07c
kevin <mattisonii@yahoo.com>
2016-04-08 14:14:56 -0700

    Add heading and navbar style

--- app/routes/question.js
+++ app/routes/question.js
@@ -3,5 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(questionParams) {
     return this.store.findRecord('question', questionParams.question_id);
+  },
+
+  saveAnswer(answerParams) {
+    var newAnswer = this.store.createRecord('answer', answerParams);
+    var title = answerParams.title;
+
+    question.get('answer').addObject(newAnswer);
+    newAnswer.saveAnswer().then(function() {
+      return title.save();
+    });
   }
 });
