27a376dfb5fdea87f822917782a5707af04005e1
kevin <mattisonii@yahoo.com>
2016-04-08 16:19:05 -0700

    Create update question functionality.

--- app/components/update-question.js
+++ app/components/update-question.js
@@ -1,4 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateQuestionForm: false,
+
+  actions: {
+    updateQuestionForm() {
+      this.set('updateQuestionForm', true);
+    },
+
+    updateQuestionFormHide() {
+      this.set('updateQuestionForm', false);
+    },
+
+    updateQuestion(question) {
+      var questionParams = {
+        title: this.get('title'),
+        user: this.get('user'),
+        subject: this.get('subject'),
+        category: this.get('category'),
+        body: this.get('body'),
+      };
+      this.set('updateQuestionForm', false);
+      this.sendAction('updateQuestion', question, questionParams);
+    }
+  }
 });
