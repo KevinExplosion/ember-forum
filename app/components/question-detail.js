8c11ca1c3f33b1f64c3dd869db2c76fbc564cbea
kevin <mattisonii@yahoo.com>
2016-04-08 16:36:22 -0700

    Question tile not working properly.

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,24 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  isQuestionDetailed: false,
   actions: {
     delete(question) {
       if (confirm('Are you sure you want to delete this question?')) {
         this.sendAction('destroyQuestion', question);
       }
-    },
-
-    updateQuestion(question, questionParams) {
-      this.sendAction('updateQuestion', question, questionParams);
-    },
-
-    questionShow: function() {
-      this.set('isQuestionDetailed', true);
-    },
-
-    questionHide: function() {
-      this.set('isQuestionDetailed', false);
     }
   }
 });
