17fc1ba1fa9c577d5c5f4d619f81e1b29b83b30a
kevin <mattisonii@yahoo.com>
2016-04-12 18:26:19 -0700

    Create computed property for question title and user name.

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isQuestionDetailed: false,
+
+  questionAndUser: Ember.computed('question.title', 'question.user', function() {
+    return this.get('question.title') + '. Submitted by: ' + this.get('question.user');
+  }),
+
   actions: {
     delete(question) {
       if (confirm('Are you sure you want to delete this question?')) {
